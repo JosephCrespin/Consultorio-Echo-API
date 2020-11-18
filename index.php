27b4c989b328bd7edf5b1905aa21d4d632e08a05
Joaquim-Frances <quimifrances@gmail.com>
2020-11-18 11:13:31 +0100

    fixed bootstrap

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 
 
-    <?php
-    require("vendor/autoload.php");
-    require("src/App.php");
+<?php
+require("vendor/autoload.php");
+require("src/App.php");
+?>
 
-    ?>
-    
+
--- src/Views/DoneConsultas.php
+++ src/Views/DoneConsultas.php
@@ -1,14 +1,17 @@
 <?php require_once("Components/Header.php");?>
 
 <body>
-<a href="?action=create">
-<button type="button" class="btn btn-outline-dark">Crear Consulta</button> 
-</a>
-
+        <a href="index.php">
+            <button type="button" id="back-button" class="btn btn-primary" aria="volver a pagina principal">
+                <svg width="3em" height="3em" viewBox="0 0 16 16" id="flecha" class="bi bi-arrow-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                </svg>
+            </button>
+        </a>
+        <h2>HISTORIAL DE CONSULTAS</h2>
   <table class="table table-hover table-dark">
     <thead>
       <tr>
-        <th scope="col">Hecho</th>
         <th scope="col">#</th>
         <th scope="col">Nombre</th>
         <th scope="col">Tema</th>
@@ -21,7 +24,6 @@
        foreach ($data["consultas"] as $consulta) 
         { echo "
                 <tr>
-                  <td>
                   <th scope='row'>{$consulta->id}</th> 
                   <td>{$consulta->name}</td>
                   <td>{$consulta->tema}</td>
@@ -37,3 +39,5 @@
     </tbody>
   </table>
 </body>
+       
+                  
